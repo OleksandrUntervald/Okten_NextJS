4c148120c5100f79c2237ce41828a2b2fb418b0c
OleksandrUntervald <untervaldthebest@gmail.com>
2025-11-06 07:37:09 +0200

    Lesson2-Fetch

--- src/services/api.services.ts
+++ src/services/api.services.ts
@@ -1,4 +1,5 @@
 import axios from "axios";
+import {IUser} from "@/moduls/IUser";
 
 const axiosInstanse = axios.create({
     baseURL: "https://jsonplaceholder.typicode.com",
@@ -9,3 +10,9 @@ export const apiGet = async <T>(url: string): Promise<T> => {
     const response = await axiosInstanse.get<T>(url);
     return response.data;
 };
+
+// export const getAllUsers = async (): Promise<IUser[]> => {
+//         const usersFetch = await fetch('https://jsonplaceholder.typicode.com/users')
+//         .then(value => value.json())
+//       return usersFetch
+// }
