73b767104cbade989f0a60b2334fb59e283fd98c
OleksandrUntervald <untervaldthebest@gmail.com>
2025-11-07 08:48:38 +0200

    Lesson2-NextJS.form

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,19 @@
 import Form from "next/form";
+import {getMeals, saveMeal} from "@/server-actions/serverActions";
 
-export default function Home() {
+export default async function Home() {
+    let mealsArray = await getMeals();
     return (
         <div className="flex min-h-screen items-center justify-center bg-white text-black">
-         <Form action='submit'>
-             <input type="text" placeholder={'login'} name={'login'} className='border-2'/>
-             <input type="text" placeholder={'password'} name={'password'} className='border-2'/>
-             <button className='border-2'>submit</button>
+         <Form action={saveMeal}>
+             <input type="text" name={'title'} className='border-2'/>
+             <button className='border-2'>send</button>
          </Form>
+            <div className={'meals'}>
+                {mealsArray.map(meal => (<div key={meal.id}>
+                    {meal.title}
+                </div>))}
+            </div>
         </div>
     );
 }
